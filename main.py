a09f497c0fb2417fca5bd268bbf52cf808d4c7d2
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-01-22 10:50:28 +0000

    New approach

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 """
 Given n as input, print the following pattern.
-Input = 4
-Output: MNOM
+Input = 8
+Output: MNOMNOMN
 """
 
-n = 4
+n = 8
 p1 = "M"
 p2 = "N"
 p3 = "O"
 st = ""
+index = 0
 for i in range(1,n+1):
-  if(i%1!=0):
-    st = st + p1
-  elif(i%2==0):
-    st = st + p2
-  elif(i%3==0):
-    st = st + p3
+  st = st + p1
+  st = st + p2
+  st = st + p3
+  index = index + 1
 print(st)
